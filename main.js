c6ac03340c547cb94294bcd3f9ddda4f08db56ce
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-30 20:05:08 +0700

    test: di vercel coba zipnya

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ const run = async () => {
             method: 'GET',
             path: '/selekda.zip',
             handler: (req, h) => {
-                return h.file('selekda.zip', {
+                return h.file(path.join(__dirname, 'selekda.zip'), {
                     mode: 'attachment'
                 })
             }
