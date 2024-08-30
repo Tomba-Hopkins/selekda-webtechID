e907dcec66fa876a050fbde2778cf9b26bce9e22
Tomba-Hopkins <saputrazamharimirza@gmail.com>
2024-08-30 19:50:48 +0700

    add: persiapan membuat zip file

---
+++ layout/app.js
@@ -0,0 +1,4 @@
+console.log('OK')
+
+const client = document.querySelector('.client').cloneNode(true)
+document.querySelector('.container').appendChild(client)
--- public/assets/script/app.js
+++ public/assets/script/app.js
@@ -2,3 +2,33 @@ console.log('OK')
 
 const client = document.querySelector('.client').cloneNode(true)
 document.querySelector('.container').appendChild(client)
+
+const hero = document.querySelector('.hero')
+
+
+const acakImage = () => {
+    let kardus = [
+        'hero1.png',
+        'hero2.jpg',
+        'hero3.jpg',
+        'hero4.jpeg',
+        'hero5.webp',
+    ]
+
+    const result = Math.floor(Math.random() * kardus.length)
+    console.log(kardus[0])
+
+    return `/img/` + kardus[result]
+}
+
+
+hero.addEventListener('click', function(){    
+    this.style.animation = 'fade 1s linear'
+    const res = acakImage()
+
+    this.src = res
+    
+    setTimeout(() => {
+        this.style.animation = 'none'
+    }, 1200)
+})
